214b316f4c48411b404e6e09de488cb48787fc4e
Travis Johnston <t239j117@ku.edu>
2015-02-28 17:31:35 -0600

    Login Updates
    Code cleanup.

--- src/com/base/irma/Login.java
+++ src/com/base/irma/Login.java
@@ -22,34 +22,24 @@ import android.widget.Toast;
 public class Login  extends AsyncTask<String,Void,String>{
 
 	public static boolean result = false;
-	public static boolean complete = false;
-	public static boolean loginsuccess;
-	public static String err;
-   private Context context;  
-   //
-   public Login(Context context) {
-      this.context = context;      
+
+   public Login(Context context) {      
    }
 
 
-protected void onPreExecute(){
-
-   }
- 
-   protected  String doInBackground(String... arg0) {    
-	   Log.i("You bitch", "i hate you");
+   protected  String doInBackground(String... arg0) {   
+	   
 	   try{
            String username = (String)arg0[0];
            String password = (String)arg0[1];		   
            String link = "http://54.69.205.117/includes/mobile_login.php?username=" + username + "&password=" + password;           
-           URL url = new URL(link);
+           new URL(link);
            HttpClient client = new DefaultHttpClient();
            HttpGet request = new HttpGet();
            request.setURI(new URI(link));
            HttpResponse response = client.execute(request);
            BufferedReader in = new BufferedReader
           (new InputStreamReader(response.getEntity().getContent()));
-         // System.out.println("###" + response.getEntity().getContent());
           StringBuffer sb = new StringBuffer("");
           String line="";
           while ((line = in.readLine()) != null) {
@@ -57,60 +47,27 @@ protected void onPreExecute(){
             break;
            }
            if(sb.toString().equals(username))
-           {
-           //Log.i("MyActivity", "MyClass.getView() — get item number " + position);
-           Log.i("YOUUUUUU", sb.toString());
-           err = "Good";
+           {           
+         
            in.close();
            result = true;
            return "true";
            }
            else
            {
-        	   Log.i("YOUUUUUU", "SUCK");
         	result = false;
-        	return "false";
-        	   
+        	return "false";        	   
            }
            
-           
-           //return sb.toString();
-           
      }catch(Exception e){
-        err = "Exception: " + e.getMessage();        
-    	 result = false;
-    	    	 
+    	 result = false;    	    	 
      }
-	//return null;
-	   complete = true;
+	
 	return null;
   }
-   
-   @Override
-   protected void onPostExecute(String result2){
-	   
-	   Success(result);
 
-   }
-   
-public static void Success(boolean result)
-{
-	//Log.i("You bitch", "i love you");
-	if(result == true)
-	{
-		//return true;
-		Log.i("You bitch", "i really love you");
-		loginsuccess = true;
-	}
-	else
-	{
-		//return false;
-		Log.i("You bitch", "go die");
-		loginsuccess = false;
-		
-	}
+
 	
 }
 
 
-}
--- src/com/base/irma/MainActivity.java
+++ src/com/base/irma/MainActivity.java
@@ -1,15 +1,5 @@
 package com.base.irma;
-//http://www.tutorialspoint.com/android/android_php_mysql.htm
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-import java.util.concurrent.TimeUnit;
-
 import android.support.v7.app.ActionBarActivity;
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,12 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.os.StrictMode;
 
 
 public class MainActivity extends ActionBarActivity {
 	
-	
+	public static boolean connection = false;
 
 	//Create some objects to use in here
 	EditText UserName;
@@ -43,40 +32,41 @@ public class MainActivity extends ActionBarActivity {
 		Password = (EditText) findViewById(R.id.password_field);
 		LoginButton = (Button) findViewById(R.id.login_button);
 		
-		LoginButton.setOnClickListener(new View.OnClickListener() {
+		LoginButton.setOnClickListener(new View.OnClickListener() 
+		{
 			
 			@Override
-			public void onClick(View v) {
-
-			Toast.makeText(MainActivity.this, "Logging in...", Toast.LENGTH_LONG).show();
-			//Connect to server with user name and password here			
-			//String res = Login.doInBackground("test");
-			user = UserName.getText().toString();
-			pw = Password.getText().toString();
-			Login(user,pw);
-			try {
-				
-					Thread.sleep(500);
-								
-			} catch(InterruptedException ex) {
-			    Thread.currentThread().interrupt();
-			}
-			Toast.makeText(MainActivity.this, "Error = " + Login.err, Toast.LENGTH_LONG).show();
-			Toast.makeText(MainActivity.this, "Boolean = " + Login.result, Toast.LENGTH_LONG).show();			
-			if(Login.result == true)
+			public void onClick(View v) 
 			{
-			//goes into the next page
-			Intent i = new Intent(MainActivity.this, MainMenu.class);
-			startActivity(i);
-			}
+				Toast.makeText(MainActivity.this, "Logging in...", Toast.LENGTH_LONG).show();
+				//Connect to server with user name and password here		
+				user = UserName.getText().toString();
+				pw = Password.getText().toString();			
+				Login(user,pw);
+				try {				
+					Thread.sleep(1000);
+				} catch(InterruptedException ex) {
+				    Thread.currentThread().interrupt();
+				}						
+				if(Login.result == true)
+				{
+					//goes into the next page
+					Intent i = new Intent(MainActivity.this, MainMenu.class);
+					startActivity(i);
+				}
+				else
+				{
+					Toast.makeText(MainActivity.this, "Invalid credentials. Please try again.", Toast.LENGTH_LONG).show();
+					
+				}
 			
 			}
 
 
-		});
+	});
 		
 		
-	}
+}
 	
 	public void Login(String user, String pw)
 	{
