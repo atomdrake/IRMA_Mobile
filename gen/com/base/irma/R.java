1a3fdde17e9bf23b3f627ee97d7bd5f7fdb0c64b
Adam <AtomDrake@gmail.com>
2015-05-01 12:55:54 -0500

    Major graphics updates, waiting on teammate feedback to complete changes.

--- gen/com/base/irma/R.java
+++ gen/com/base/irma/R.java
@@ -1632,14 +1632,20 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int ic_launcher=0x7f020033;
-        public static final int irma_temp_logo=0x7f020034;
-        public static final int item_add_background=0x7f020035;
-        public static final int left_arrow=0x7f020036;
-        public static final int rectangle=0x7f020037;
-        public static final int right_arrow=0x7f020038;
-        public static final int rounded_edittext=0x7f020039;
-        public static final int x=0x7f02003a;
+        public static final int brown_wood_texture=0x7f020033;
+        public static final int ic_launcher=0x7f020034;
+        public static final int irma_logo=0x7f020035;
+        public static final int irma_temp_logo=0x7f020036;
+        public static final int item_add_background=0x7f020037;
+        public static final int left_arrow=0x7f020038;
+        public static final int light_brown_wood_texture=0x7f020039;
+        public static final int rectangle=0x7f02003a;
+        public static final int right_arrow=0x7f02003b;
+        public static final int round_button_large=0x7f02003c;
+        public static final int round_button_med=0x7f02003d;
+        public static final int round_button_small=0x7f02003e;
+        public static final int rounded_edittext=0x7f02003f;
+        public static final int x=0x7f020040;
     }
     public static final class id {
         public static final int Aisle_field=0x7f0a0069;
